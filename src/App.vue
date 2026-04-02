d969b3a5096af39ebdec16679c5cc794c4e9f1f6
kurosawa <kurosawa_keisuke@care-con.co.jp>
2026-04-02 18:27:55 +0900

    データを取得し画面表示

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+const post = ref(null)
+
+async function fetchdata() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+  post.value = await res.json()
+}
+
+fetchdata()
+</script>
+
+<template>
+  <div>
+    <p>ID: {{ post.id }}</p>
+    <p>タイトル: {{ post.title }}</p>
+    <p>本文: {{ post.body }}</p>
+  </div>
+</template>
